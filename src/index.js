04ae0f02cb11cd37f6cba0f9e5789d53041e8df6
raspi-user <pi-cables@outlook.com>
2022-08-25 00:37:50 +0200

    Added config defaults.js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import Prompt from '@babbage/react-prompt'
+
 ReactDOM.render(
   <Prompt appName='NanoStore UI'>
     <App />
