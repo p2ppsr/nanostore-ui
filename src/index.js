429f158029d722ca2cf00d83cfde3e6d3525c14b
raspi-user <pi-cables@outlook.com>
2022-08-24 23:37:34 +0200

    Test branch update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import Prompt from '@babbage/react-prompt'
-
 ReactDOM.render(
   <Prompt appName='NanoStore UI'>
     <App />
